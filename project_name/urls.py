ae6b531ff0c95340b4f224d8222ee940c2eedf72
Diederik van der Boor <vdboor@edoburu.nl>
2013-07-05 10:44:28 +0200

    Add missing TinyMCE URLs

--- project_name/urls.py
+++ project_name/urls.py
@@ -22,8 +22,9 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/filebrowser/', include(fb_site.urls)),
-    url(r'^admin/util/tools/', include('admin_tools.urls')),
     url(r'^admin/util/tags/', include('taggit_autocomplete_modified.urls')),
+    url(r'^admin/util/tools/', include('admin_tools.urls')),
+    url(r'^admin/util/tinymce/', include('tinymce.urls')),
 
     # Test pages
     url(r'^500test/$', view=Http500View.as_view()),

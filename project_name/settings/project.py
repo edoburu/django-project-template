cc2a00e35a9973bb73692291706eee315435ec25
Diederik van der Boor <vdboor@edoburu.nl>
2015-12-31 13:37:23 +0100

    Bump requirements

--- project_name/settings/project.py
+++ project_name/settings/project.py
@@ -67,6 +67,10 @@ TEMPLATE_CONTEXT_PROCESSORS += (
     'frontend.context_processors.frontend',
 )
 
+TEMPLATE_LOADERS += (
+    'admin_tools.template_loaders.Loader',
+)
+
 FORMAT_MODULE_PATH = '{{ project_name }}.settings.locale'  # Consistent date formatting
 
 # Avoid 600 permission for filebrowser uploads.

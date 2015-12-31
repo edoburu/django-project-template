0641426172995203ba4bd6ecc02f6a5391ae2edc
Diederik van der Boor <vdboor@edoburu.nl>
2015-12-31 13:52:59 +0100

    Remove unused ENTRY_DATETIME_FORMAT

--- project_name/settings/locale/nl/formats.py
+++ project_name/settings/locale/nl/formats.py
@@ -9,5 +9,3 @@ This file is read because of the following setting:
 
 DATETIME_FORMAT = 'j F Y \o\m H:i'      # '20 januari 2009 om 15:23'
 #DATETIME_FORMAT = 'j F Y, H:i \u\u\\r'  # '20 januari 2009, 15:23 uur'
-
-ENTRY_DATETIME_FORMAT = 'l j F Y, H:i \u\u\\r'

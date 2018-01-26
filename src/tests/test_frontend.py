4b428808afe810d8a64268bcaa8be9b745103563
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-26 15:56:06 +0100

    Improve tests, add browserconfig example

---
+++ src/tests/test_frontend.py
@@ -0,0 +1,27 @@
+"""Test the frontend code"""
+import pytest
+
+
+@pytest.mark.django_db
+def test_error_pages(client):
+    response = client.get('/404/')
+    assert response.status_code == 404
+
+    response = client.get('/500/')
+    assert response.status_code == 500
+
+
+def test_serve_web_file(client):
+    response = client.get('/browserconfig.xml')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_sitemap(client):
+    response = client.get('/sitemap.xml')
+    assert response.status_code == 200
+
+
+def test_robots(client):
+    response = client.get('/robots.txt')
+    assert response.status_code == 200

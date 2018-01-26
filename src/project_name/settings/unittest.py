f2b573ba8c01990ce120a59b0beea5876d78b700
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-26 14:29:27 +0100

    improve unittest settings

--- src/project_name/settings/unittest.py
+++ src/project_name/settings/unittest.py
@@ -4,11 +4,26 @@ import sys
 if 'runtests.py' in sys.argv[0] or 'test' in sys.argv:
     print("Using in memory database.\n")
 
+CACHES={
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    },
+}
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.sqlite3',
     #'NAME': dirname(dirname(dirname(__file__))) + '/testdatabase.db'
 }
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
 
 COMPRESS_ENABLED = True
 
